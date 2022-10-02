e1f9adcc6593ba80060b27f95298b12b6f6ce130
Sam Lee <sam.lee@myhunter.cuny.edu>
2022-10-02 15:31:25 -0400

    fix doctest and finish task a

--- funcs.cpp
+++ funcs.cpp
@@ -3,3 +3,26 @@
 
 // add functions here
 
+bool isDivisibleBy(int n, int d)
+{
+  if (n < d){
+    return false;
+  } else if (n % d == 0) {
+    return true;
+  }
+  return false;
+}
+
+bool isPrime(int n){
+  if (n < 2){
+    return false;
+  }
+  for (int i = 2; i < n; i++){
+    if (isDivisibleBy(n, i)){
+      return false;
+    }
+  }
+}
+
+
+  
--- tests.cpp
+++ tests.cpp
@@ -3,3 +3,17 @@
 #include "funcs.h"
 
 // add your tests here
+TEST_CASE("IsDivisibleBy Function"){
+  CHECK(isDivisibleBy(0, 10) == false);
+  CHECK(isDivisibleBy(20, 10) == true);
+  CHECK(isDivisibleBy(5, 10) == false);
+}
+
+TEST_CASE("isPrime Function"){
+  CHECK(isPrime(-1) == false);
+  CHECK(isPrime(1) == false);
+  CHECK(isPrime(2) == true);
+  CHECK(isPrime(19) == true);
+  CHECK(isPrime(47) == true);
+}
+
